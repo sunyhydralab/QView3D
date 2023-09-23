60fcb74041f8e1e04ab0c021d9cbe767e2c7a743
ItBeCharlie <charlied713@gmail.com>
2023-09-23 15:46:00 -0400

    Initalized various data structures and classes

--- printerClass.py
+++ printerClass.py
@@ -1,27 +1,5 @@
-# Class for each printer job. 
-class Job:
-
-    def __init__(self, name, file, quantity, position, printers):
-        self.name = name
-        self.file = file
-        self.quantity = quantity
-        self.position = position
-        self.printers = printers
-        self.status = ""
-        #If printer is makerbot
-            self.convertFile()
-        #else
-        self.printFile()
-        
-
-    def convertFile(self):
-        
-    def printFile(self):
-    
-
 # Class for each printer.
 class Printer:
-
     def __init__(self, name, address, status):
         self.name = name
         self.address = address
