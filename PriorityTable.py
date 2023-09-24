54c3167a8628099e8fa70d45154e4a84a7fb3a74
ItBeCharlie <charlied713@gmail.com>
2023-09-23 21:28:40 -0400

    Added logic to remove job queue from table

--- PriorityTable.py
+++ PriorityTable.py
@@ -20,6 +20,33 @@ class PriorityTable:
         queue_list.append(item)
         # Have to check if this will update the list or if I have to manually re set the tuple
 
+    def remove_job_queue(self, item):
+        """
+        Removes the specified item from self.__table
+        Returns True if the item is no longer in the table
+        Returns False if item was not found in the table
+        """
+        tuple = self.__table.get(item.get_priority())
+        # If item not in table, don't check further
+        if tuple == None:
+            return False
+        queue_list, next_turn = tuple
+        # Iterate through all queues, and find the one with a matching id to our item to remove
+        for queue in queue_list:
+            if queue.get_id() == item.get_id():
+                # Remove the item from queue list
+                queue_list.remove(queue)
+                # Check if queue list for this priority is now empty, if so, remove priority from table
+                if len(queue_list) == 0:
+                    self.__table.remove(item.get_priority())
+                    return True
+                # Fix the next_turn index
+                next_turn %= len(queue_list)
+                # Re set the table at the priority we just removed from
+                self.__table.set(item.get_priority(), (queue_list, next_turn))
+                return True
+        return False
+
     def __add_key(self, new_key):
         # Add key to sorted_keys list, could use binary search in the future
         for index in range(self.__sorted_keys):
