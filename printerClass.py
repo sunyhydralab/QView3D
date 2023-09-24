d7a80eba6b5605ba829f58872ed8d183810dbc6b
ItBeCharlie <charlied713@gmail.com>
2023-09-23 22:43:15 -0400

    Renamed variables to be indicated as Private

--- printerClass.py
+++ printerClass.py
@@ -1,9 +1,9 @@
 # Class for each printer.
 class Printer:
     def __init__(self, name, address, status):
-        self.name = name
-        self.address = address
-        self.status = status
+        self.__name = name
+        self.__address = address
+        self.__status = status
 
-    def getStatus(self):
-        return self.status
+    def get_status(self):
+        return self.__status
