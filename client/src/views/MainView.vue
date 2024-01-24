e25925fcc5c35a3d90785f5f2ca038cfd6f3869d
Lara-Codes <lara.palombi@gmail.com>
2024-01-24 10:19:30 -0500

    main page outline

--- client/src/views/MainView.vue
+++ client/src/views/MainView.vue
@@ -1,8 +1,62 @@
+<script setup lang="ts">
+  import { useRetrievePrinters, type Device } from '@/model/ports';
+import { onMounted, ref } from 'vue';
+  const {retrieve} = useRetrievePrinters(); 
+
+  let devices = ref<Array<Device>>([]); // Array of all devices. Used to list registered printers on frontend. 
+
+  onMounted(async () => {
+    try{
+        const registeredList = await retrieve()//list of previously registered printers 
+        devices.value = registeredList; // loads registered printers into registered array 
+    }catch(error){
+      console.error(error)
+    }
+  })
+
+</script>
+
 <template>    
   <div class="container">
-      <p>Main View</p>
-  </div>
+    <b>Home</b>
+        <table>
+          <!-- This data is taken from the backend in-memory array, NOT the database. Done with threading. -->
+          <!-- Perhaps the printer names are displayed and this is drawn from the database, but all of the associated data
+              is taken from the in-memory threads and cron updates. 
+          -->
+            <tr>
+                <th>Printer name</th> 
+                <th>Status</th>
+                <th>Job Name</th>
+                <th>File</th>
+                <th>Progress</th>
+            </tr>
+            <tr v-for="device in devices">
+                <th>{{ device.name }}</th> 
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </table>
+    </div>
 </template>
 
-<style>
+<style scoped>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+th,
+td {
+    border: 2px solid #dddddd;
+    /* Set border width and color */
+    text-align: left;
+    padding: 8px;
+}
+
+th {
+    background-color: #f2f2f2;
+}
 </style>
--- client/src/views/SubmitJob.vue
+++ client/src/views/SubmitJob.vue
@@ -9,8 +9,55 @@
 //     file.value = fileInput.value?.files
 // }
 </script>
-<template>    
+<template>
     <div class="container">
         <p>Submit Job View</p>
+
+        <div class="form-container">
+            <form>
+                <select required> 
+                    <!-- Pull devices from Printer in database -->
+                    <option value="None">Device: None</option>
+                </select>
+                <br><br>
+                Upload your .gcode file
+                <!-- Decide which file types are compatible with which printer. .gcode v-if printer is compatible with .gcode, .x3g if with .x3g, etc -->
+                <input type="file" id="file" name="file" accept=".gcode" required>
+                <br><br>
+
+                <!-- Make it so user can't insert negative quantity. Decide on upper limit. -->
+                <!-- Make load-balancing feature -->
+                <label for="name">Quantity</label>
+                <input type="number" id="quantity" name="quantity" required>
+                <br><br>
+
+                <label for="priority">Priority job?</label>
+                <input type="checkbox" id="priority" name="priority">
+
+                <br><br>
+                <!-- This form data does NOT go into the database -- goes into in-memory printer array to be handled. 
+                    Also is sent to the QueueView. Perhaps the form is sent to the backend and queueview retrieves that data
+                    from the backend. 
+                    The job data only gets inserted into the Job table once it is done printing. 
+                -->
+                <input type="submit" value="Submit">
+            </form>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.form-container {
+    border: 2px solid #333;
+    padding: 20px;
+    margin-top: 20px;
+    width: 300px;
+}
+</style>
