cdaed7f9db753a4b833514eeb44fd228f0b04fbf
CJ <iron768gamer@gmail.com>
2024-09-23 16:41:32 -0400

    chore: clean up unused files and unneeded comments

--- run_prod.sh
+++ run_prod.sh
@@ -4,10 +4,6 @@
 run_prod() {
     echo "Starting the production server..."
     cd server
-    #uvicorn asgi:flask_app --host 0.0.0.0 --port 8000
-    #waitress-serve --call --port=8000 'app:create_app'
-    # restful webservice 
-    #hypercorn 'app:create_app' --bind 0.0.0.0:8000 --workers 4
     gunicorn --worker-class eventlet -w 1 app:app
 }
 
