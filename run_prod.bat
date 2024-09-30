e8d851e6cc092dc5ce71a83b5efc5318ff8ecbbd
Nathan Gopee <73437724+ndg8743@users.noreply.github.com>
2024-09-30 11:52:35 -0400

    fix: fix windows run_prod batch script

--- run_prod.bat
+++ run_prod.bat
@@ -1,11 +1,26 @@
 @echo off
+setlocal enabledelayedexpansion
 
+:env
+for /f "usebackq tokens=1,* delims==" %%a in ("server/.env") do (
+    set "%%a=%%b"
+)
+
+:build
 echo Building the client...
 cd client
-npm run build-only
+call npm run build-only
+if errorlevel 1 (
+    echo Build failed. Exiting...
+    exit /b 1
+)
+goto run
 
-cd ..
-
+:run
 echo Starting the production server...
-cd server
-gunicorn --worker-class eventlet -w 1 app:app
+cd ../server
+call gunicorn --bind=%FLASK_RUN_HOST%:%FLASK_RUN_PORT% --worker-class eventlet -w 1 app:app
+if errorlevel 1 (
+    echo Server failed to start. Exiting...
+    exit /b 1
+)
