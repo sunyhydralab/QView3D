5e373bd105c0495c7457b75013b9767c4c279415
L10nhunter <yegera1@newpaltz.edu>
2024-10-08 19:04:55 -0400

    updated test.py

--- server/test.py
+++ server/test.py
@@ -1,7 +1,18 @@
-from Classes import Queue
+import serial.tools.list_ports
+from Classes.Vector3 import Vector3
 
-test = Queue()
+homeLocation = Vector3(14.0,-4.0,2.0)
 
-test.addToFront(5)
-
-print(test.queue)
+printer = serial.Serial("ttyACM0", 115200, timeout=10)
+printer.write("G28\n".encode("utf-8"))
+printer.write("M114\n".encode("utf-8"))
+while True:
+    line = printer.readline()
+    try:
+        print(line)
+        if homeLocation.__repr__() in line.decode("utf-8"):
+            break
+    except KeyboardInterrupt:
+        break
+printer.write("M84\n".encode("utf-8"))
+printer.close()
