24e1122bb10209ae24a70484cc62586215513273
L10nhunter <yegera1@newpaltz.edu>
2024-11-11 17:54:44 -0500

    feat: added the changeFilament and changeNozzle methods

--- server/Classes/Fabricators/Printers/Printer.py
+++ server/Classes/Fabricators/Printers/Printer.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta
+from app import app
 from Classes.Fabricators.Device import Device
 
 class Printer(Device, metaclass=ABCMeta):
@@ -8,5 +9,27 @@ class Printer(Device, metaclass=ABCMeta):
     bedTemperature: int | None = None
     nozzleTemperature: int | None = None
 
-    def changeFilament(self):
-        pass
+    def changeFilament(self, filamentType: str, filamentDiameter: float):
+        if not isinstance(filamentDiameter, float):
+            filamentDiameter = float(filamentDiameter)
+        try:
+            if self.status is "idle":
+                self.filamentType = filamentType
+                self.filamentDiameter = filamentDiameter
+            else:
+                raise Exception("Printer is not idle")
+        except Exception as e:
+            if self.logger is None:
+                with app.app_context():
+                    app.logger.error("Error changing filament:")
+                    app.logger.error(e)
+            else:
+                self.logger.error("Error changing filament:")
+                self.logger.error(e)
+        self.filamentType = filamentType
+        self.filamentDiameter = filamentDiameter
+
+    def changeNozzle(self, nozzleDiameter: float):
+        if not isinstance(nozzleDiameter, float):
+            nozzleDiameter = float(nozzleDiameter)
+        self.nozzleDiameter = nozzleDiameter
