743b956f9cbc103bc66b9995e2ca2355fd3e7528
L10nhunter <yegera1@newpaltz.edu>
2024-11-13 14:17:18 -0500

    fix: updated to have proper logging and env setup.

--- server/app.py
+++ server/app.py
@@ -1,34 +1,47 @@
-from flask import Flask, jsonify, request, Response, url_for, send_from_directory
-from threading import Thread
-from flask_cors import CORS 
-import os 
+from flask import Flask, request, Response, send_from_directory
+from flask_cors import CORS
+import os
 from models.db import db
-from models.printers import Printer
 from models.PrinterStatusService import PrinterStatusService
 from flask_migrate import Migrate
-from dotenv import load_dotenv, set_key
-from controllers.ports import getRegisteredPrinters
+from dotenv import load_dotenv
+from controllers.ports import getRegisteredFabricators
 import shutil
 from flask_socketio import SocketIO
-from datetime import datetime, timedelta
-from sqlalchemy import text
-import json
 from models.config import Config
-
 
 
 # moved this up here so we can pass the app to the PrinterStatusService
 # Basic app setup 
 app = Flask(__name__, static_folder='../client/dist')
-app.config.from_object(__name__) # update application instantly 
+app.config.from_object(__name__) # update application instantly
+logs = os.path.join('./logs')
+from Classes.Logger import Logger
+app.logger = Logger("App", consoleLogger=None, fileLogger=os.path.join(logs, "app.log"))
+# start database connection
+app.config["environment"] = Config.get('environment')
+app.config["ip"] = Config.get('ip')
+app.config["port"] = Config.get('port')
+app.config["base_url"] = Config.get('base_url')
 
+load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+database_file = os.path.join(basedir, Config.get('database_uri'))
+if isinstance(database_file, bytes):
+    database_file = database_file.decode('utf-8')
+databaseuri = 'sqlite:///' + database_file
+app.config['SQLALCHEMY_DATABASE_URI'] = databaseuri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+migrate = Migrate(app, db)
 # moved this before importing the blueprints so that it can be accessed by the PrinterStatusService
 printer_status_service = PrinterStatusService(app)
 
 # Initialize SocketIO, which will be used to send printer status updates to the frontend
-# and this specific socketit will be used throughout the backend
+# and this specific socket it will be used throughout the backend
 
-if Config.get('environment') == 'production':
+if app.config["environment"] == 'production':
     async_mode = 'eventlet'  # Use 'eventlet' for production
 else:
     async_mode = 'threading'  # Use 'threading' for development
@@ -36,11 +49,23 @@ else:
 socketio = SocketIO(app, cors_allowed_origins="*", engineio_logger=False, socketio_logger=False, async_mode=async_mode) # make it eventlet on production!
 app.socketio = socketio  # Add the SocketIO object to the app object
 
-# IMPORTING BLUEPRINTS 
-from controllers.ports import ports_bp
-from controllers.jobs import jobs_bp
-from controllers.statusService import status_bp, getStatus 
-from controllers.issues import issue_bp
+def handle_errors_and_logging(e: Exception, fabricator = None):
+    from Classes.Fabricators.Fabricator import Fabricator
+    device = fabricator
+    if isinstance(fabricator, Fabricator):
+        device = fabricator.device
+    if device.logger is None:
+        if app.logger is None:
+            print(e)
+        else:
+            app.logger.error("Error:")
+            app.logger.error(e)
+    else:
+        device.logger.error("Error:")
+        device.logger.error(e)
+    return True
+
+app.handle_error_and_logging = handle_errors_and_logging
 
 CORS(app)
 
@@ -63,16 +88,11 @@ def serve_static(path='index.html'):
 def serve_assets(filename):
     return send_from_directory(os.path.join(app.static_folder, 'assets'), filename)
 
-# start database connection
-load_dotenv()
-basedir = os.path.abspath(os.path.dirname(__file__))
-database_file = os.path.join(basedir, Config.get('database_uri'))
-databaseuri = 'sqlite:///' + database_file
-app.config['SQLALCHEMY_DATABASE_URI'] = databaseuri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-migrate = Migrate(app, db)
+# IMPORTING BLUEPRINTS
+from controllers.ports import ports_bp
+from controllers.jobs import jobs_bp
+from controllers.statusService import status_bp
+from controllers.issues import issue_bp
 
 # # Register the display_bp Blueprint
 app.register_blueprint(ports_bp)
@@ -87,15 +107,15 @@ def handle_ping():
 # own thread
 with app.app_context():
     try:
+        # Create in-memory uploads folder
+        uploads_folder = os.path.join('../uploads')
+        tempcsv = os.path.join('../tempcsv')
+
         # Creating printer threads from registered printers on server start 
-        res = getRegisteredPrinters() # gets registered printers from DB 
+        res = getRegisteredFabricators() # gets registered fabricators from DB
         data = res[0].get_json() # converts to JSON 
         printers_data = data.get("printers", []) # gets the values w/ printer data
         printer_status_service.create_printer_threads(printers_data)
-        
-        # Create in-memory uploads folder 
-        uploads_folder = os.path.join('../uploads')
-        tempcsv = os.path.join('../tempcsv')
 
         if os.path.exists(uploads_folder):
             # Remove the uploads folder and all its contents
@@ -106,14 +126,14 @@ with app.app_context():
             os.makedirs(uploads_folder)
             os.makedirs(tempcsv)
 
-            print("Uploads folder recreated as an empty directory.")
+            app.logger.info("Uploads folder recreated as an empty directory.")
         else:
             # Create the uploads folder if it doesn't exist
             os.makedirs(uploads_folder)
             os.makedirs(tempcsv)
-            print("Uploads folder created successfully.")  
+            app.logger.info("Uploads folder created successfully.")
     except Exception as e:
-        print(f"Unexpected error: {e}")
+        app.logger.error(f"Unexpected error: {e}")
             
 
 if __name__ == "__main__":
@@ -123,6 +143,3 @@ if __name__ == "__main__":
     # since we are using socketio, we need to use socketio.run instead of app.run
     # which passes the app anyways
     socketio.run(app, debug=True)  # Replace app.run with socketio.run
-    
-def create_app():
-    return app
