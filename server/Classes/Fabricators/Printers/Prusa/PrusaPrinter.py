2ded9e67311990a43213c32cd08093b44d4dc539
L10nhunter <yegera1@newpaltz.edu>
2024-11-13 13:38:47 -0500

    fix: updated to use Prusa Gcode instead of Marlin

--- server/Classes/Fabricators/Printers/Prusa/PrusaPrinter.py
+++ server/Classes/Fabricators/Printers/Prusa/PrusaPrinter.py
@@ -2,31 +2,30 @@ from abc import ABCMeta, abstractmethod
 from typing_extensions import Buffer
 from Classes.Vector3 import Vector3
 from Classes.Fabricators.Printers.Printer import Printer
+from Mixins.gcode.usesPrusaGcode import usesPrusaGcode
 from Mixins.hasEndingSequence import hasEndingSequence
-from Mixins.gcode.usesMarlinGcode import usesMarlinGcode
 
 
-class PrusaPrinter(Printer, hasEndingSequence, usesMarlinGcode, metaclass=ABCMeta):
+class PrusaPrinter(Printer, hasEndingSequence, usesPrusaGcode, metaclass=ABCMeta):
     VENDORID = 0x2C99
 
-
     def sendGcode(self, gcode: Buffer, isVerbose: bool = False):
-        return usesMarlinGcode.sendGcode(self, gcode, isVerbose)
+        return usesPrusaGcode.sendGcode(self, gcode, isVerbose)
 
     def parseGcode(self, file, isVerbose=False):
-        return usesMarlinGcode.parseGcode(self, file, isVerbose)
+        return usesPrusaGcode.parseGcode(self, file, isVerbose)
 
     def connect(self):
-        return usesMarlinGcode.connect(self)
+        return usesPrusaGcode.connect(self)
 
     def disconnect(self):
-        usesMarlinGcode.disconnect(self)
+        usesPrusaGcode.disconnect(self)
 
     def home(self, isVerbose: bool = False):
-        return usesMarlinGcode.home(self, isVerbose)
+        return usesPrusaGcode.home(self, isVerbose)
 
     def goTo(self, loc: Vector3, isVerbose: bool = False):
-        return usesMarlinGcode.goTo(self, loc, isVerbose)
+        return usesPrusaGcode.goTo(self, loc, isVerbose)
 
     @abstractmethod
     def endSequence(self):
@@ -37,4 +36,4 @@ class PrusaPrinter(Printer, hasEndingSequence, usesMarlinGcode, metaclass=ABCMet
         pass
 
     def getToolHeadLocation(self) -> Vector3:
-        return usesMarlinGcode.getToolHeadLocation(self)
+        return usesPrusaGcode.getToolHeadLocation(self)
