97e7371d67e87b703e550406b065da0d9e1dff61
Yoosup Song <Youssupsong@gmail.com>
2025-02-21 14:51:15 -0500

    fix: Frontend not reloading
    Refer to Issue #113

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,6 +10,7 @@ import { onMounted } from 'vue';
 import {setupSockets} from '@/model/sockets';
 import {useRetrievePrintersInfo, printers} from '@/model/ports';
 import {isLoading, setupTimeSocket} from '@/model/jobs';
+import {watch} from 'vue';
 
 const { retrieveInfo } = useRetrievePrintersInfo();
 
@@ -19,6 +20,14 @@ onMounted(async () => {
   // sockets
   setupSockets(printers.value)
   setupTimeSocket(printers.value)
+})
+
+// For updating the frontend when information on the printer changes
+watch(printers, (updatedPrinter) => {
+  if (updatedPrinter) {
+    setupSockets(printers.value)
+    setupTimeSocket(printers.value)
+  }
 })
 </script>
 
