80cd63e101a8481ffac757e1ef1887b0a7add4ee
Youssup <youssupsong@gmail.com>
2025-04-16 10:52:47 -0400

    feat: Setup basic flask server and env

---
+++ server/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from pathlib import Path
+
+#load .env file from root directory
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / '.env')
+
+# create and configure the app
+app = Flask(__name__)
+
+# Load configuration from environment variables
+app.config.from_mapping(
+    SECRET_KEY=os.getenv('SECRET_KEY'),
+    DATABASE=os.getenv('DATABASE')
+)
+
+@app.route('/')
+def test():
+    return 'Server is running'
