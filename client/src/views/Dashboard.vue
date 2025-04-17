2f82a520c25d2e66b24724fdaecb654ad3d9002c
Yoosup Song <Youssupsong@gmail.com>
2025-04-17 14:16:08 -0400

    fix: wrong class name

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 className="text-2xl font-bold underline dark:text-8xl">Hello world!</h1>
+  <h1 class="text-2xl font-bold underline dark:text-8xl">Hello world!</h1>
 </template>
 
 <style scoped></style>
