301f2ec25a91952aed9a4951ca2c4b95c97e5ae5
Yoosup Song <Youssupsong@gmail.com>
2025-04-17 13:55:48 -0400

    chore: delete MainView.vue

--- client/src/views/MainView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="dark:bg-gray-800">
-    <h1 className="text-2xl font-bold underline">Hello world!</h1>
-  </div>
-</template>
-
-<style scoped></style>
