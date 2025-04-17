40475d6a930a45d7c6cf235b4fa4f69f9380c4e8
Youssup <youssupsong@gmail.com>
2025-04-17 01:38:50 -0400

    feat: created navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
+  <Navbar />
   <RouterView />
 </template>
 
