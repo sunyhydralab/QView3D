0d2b10eb68954d8412720bee9df4df0e7c969f00
Yoosup Song <Youssupsong@gmail.com>
2025-04-16 21:03:22 -0400

    feat: setup Vue frontend without boilerplate

---
+++ client/src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/MainView.vue'),
+    },
+  ],
+})
+
+export default router
