c662102728a0899cffa63e4df89bd9c282a6e9f8
Yoosup Song <Youssupsong@gmail.com>
2025-04-17 16:37:50 -0400

    feat: added registerFabricator function

--- client/src/models/Fabricator.ts
+++ client/src/models/Fabricator.ts
@@ -1,6 +1,6 @@
 import { ref } from 'vue'
 
-export const fabrictors_list = ref<Fabricator[]>([])
+export const fabrictorList = ref<Fabricator[]>([])
 
 interface Fabricator 
 {
@@ -15,12 +15,28 @@ interface Fabricator
 	isWaitingForResponse : boolean
 }
 
+// Sets the fabricator information, adds a fabricator to the fabricatorList, and returns a fabricator
+export function registerFabricator(userSubmission : {customName: string; description: string; modelName: string, date: number;}) : Fabricator
+{
+	const newFabricator : Fabricator = {
+		customName: userSubmission.customName,
+		description: userSubmission.description,
+		dateRegistered: userSubmission.date,
+		modelName: userSubmission.modelName,
+		jobQueue: [],
+		isQueueExpanded : false,
+		isWaitingForResponse : false
+
+	}
+	fabrictorList.value.push(newFabricator)
+	return newFabricator
+}
+
 /*
 WIP
 add_job(fabricator_id : number, job : Job)
 remove_job(fabricator_id : number, job_id : number)
 get_job_status(fabricator_id: number)
-register_fabricator(fabricator : Fabricator)
 edit_job_status(fabricator_id: number, job_status : string)
 move_job(fabricator_id: number, job_id : number, new_position : number)
 edit_name(fabricator_id: number, custom_name : string)
