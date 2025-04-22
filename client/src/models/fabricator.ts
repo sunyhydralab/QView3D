8b64536c7cfadd8b6c697a99cf1ceacc05da9c2d
Yoosup Song <Youssupsong@gmail.com>
2025-04-21 21:42:04 -0400

    feat: Registering printers + more verbose naming

--- client/src/models/fabricator.ts
+++ client/src/models/fabricator.ts
@@ -3,31 +3,37 @@ import { type Job } from '@/models/job'
 import { api } from '@/models/api'
 
 export interface Fabricator {
-    device: Record<string, any>
-    description: string
-    hwid: string
-    name?: string
-    status?: string
-    date?: Date
-    id?: number
-    error?: string
-    canPause?: number
-    queue?: Job[] //  Store job array to store queue for each printer.
-    isQueueExpanded?: boolean
-    isInfoExpanded?: boolean
-    extruder_temp?: number
-    bed_temp?: number
-    colorChangeBuffer?: number
-    colorbuff?: number,
-  }
+  device: Record<string, any>
+  description: string
+  hwid: string
+  name?: string
+  status?: string
+  date?: Date
+  id?: number
+  error?: string
+  canPause?: number
+  queue?: Job[] //  Store job array to store queue for each printer.
+  isQueueExpanded?: boolean
+  isInfoExpanded?: boolean
+  extruder_temp?: number
+  bed_temp?: number
+  colorChangeBuffer?: number
+  colorbuff?: number,
+}
 
 
 export const FabricatorList = ref<Fabricator[]>([])
 
-export async function getPorts() {
-    return api('getports');
+export async function getConnectedFabricators() {
+  return api('getports');
 }
 
 export async function retrievePrinters() {
-      return await api('getprinterinfo')
+  return await api('getprinterinfo')
+}
+
+export async function registerFabricator(fabricator: Fabricator) {
+  // set fabricator to printer because the api expects they key, 'printer'
+  const printer = fabricator
+  return await api('register', { printer })
 }
--- client/src/views/Registration.vue
+++ client/src/views/Registration.vue
@@ -1,37 +1,51 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { type Fabricator, getPorts, retrievePrinters } from '@/models/fabricator';
+import { type Fabricator, getConnectedFabricators, registerFabricator } from '@/models/fabricator';
 
+// all connected fabricators
 const connectedFabricatorList = ref<Fabricator[]>([]);
-const selectedDevice = ref<Fabricator | null>(null);
+
+// the selected fabricator from the dropdown
+const selectedFabricator = ref<Fabricator | null>(null);
+
+// the user submitted name for the fabricator
+const customName = ref<string | null>(null);
 
 // fetch list of connected ports from backend and automatically load them into the form dropdown 
 onMounted(async () => {
-  connectedFabricatorList.value = await getPorts();
+  connectedFabricatorList.value = await getConnectedFabricators();
 });
+
+async function handleSubmit() {
+  if(selectedFabricator.value && customName.value) {
+    selectedFabricator.value.name = customName.value;
+    console.log('Selected Fabricator:', selectedFabricator.value);
+    await registerFabricator(selectedFabricator.value);
+  }
+}
 </script>
 
 <template>
   <div class="mx-auto max-w-md space-y-4">
-  <!-- Select Device -->
+  <!-- Select Fabricator -->
   <div>
-    <label for="deviceSelect" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-100">Select a Device</label>
-    <select id="deviceSelect" v-model="selectedDevice"
+    <label for="fabricatorSelect" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-100">Select a Fabricator</label>
+    <select id="fabricatorSelect" v-model="selectedFabricator"
       class="block w-full px-4 py-2 bg-light-primary-light border-light-primary-dark rounded-md shadow-sm focus:outline-none focus:ring-2 focus:border-accent-primary-light focus:ring-accent-primary-light"
       required>
-      <option disabled value="">Select a device</option>
+      <option disabled value="">Select a fabricator</option>
       <option v-for="fabricator in connectedFabricatorList" :key="fabricator.device.dbID" :value="fabricator">
         {{ fabricator.description }}
       </option>
     </select>
   </div>
 
-  <!-- Device Name -->
+  <!-- Fabricator Name -->
   <div class="space-y-1">
-    <label for="name" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-100">Device Name</label>
+    <label for="name" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-100">Fabricator Name</label>
     <input id="name" type="text"
       class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:border-accent-primary-light focus:ring-accent-primary-light"
-      placeholder="Name your device" />
+      v-model="customName" placeholder="Name your fabricator" />
   </div>
 
   <!-- Description
@@ -47,12 +61,13 @@ onMounted(async () => {
     <label for="model" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-100">Model</label>
     <input id="model" type="text"
       class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm bg-gray-100 cursor-not-allowed"
-      disabled :placeholder="selectedDevice?.description"/>
+      disabled :placeholder="selectedFabricator?.description"/>
   </div>
 
   <!-- Submit Button -->
   <button type="submit"
-    class="w-full bg-accent-primary hover:bg-accent-primary-dark text-white py-2 px-4 rounded-md transition duration-300">
+    class="w-full bg-accent-primary hover:bg-accent-primary-dark text-white py-2 px-4 rounded-md transition duration-300 disabled:bg-accent-primary-light disabled:cursor-not-allowed" :disabled="!selectedFabricator"
+    :onclick="handleSubmit">
     Submit
   </button>
 </div>
