71d2cdf03a6477c1cb25cedd5e31b919bc2ff2f7
Ari Yeger <yegera1@newpaltz.edu>
2025-04-24 14:42:11 -0400

    fix: stop navbar from scrolling

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,8 +4,6 @@ import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
-  <Navbar />
-  <RouterView class="pt-10 px-10 min-h-screen h-full"/>
   <nav class="fixed top-0 left-0 right-0 z-50shadow-md">
     <Navbar />
   </nav>
