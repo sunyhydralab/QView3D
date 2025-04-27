625654d839631713431eb298e6453d51eb4429dc
Ari Yeger <yegera1@newpaltz.edu>
2025-04-27 15:49:59 -0400

    dep: temps for sockets, for modeling

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,6 @@
 import { setModeToSystem } from './composables/useMode'
+import { setupSockets } from '@/composables/useWebSockets.ts'
+import { retrieveRegisteredFabricators } from '@/models/fabricator.ts'
 import './assets/base.css'
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -11,6 +13,9 @@ setModeToSystem()
 
 // Initialize socket connection
 connectSocket()
+
+// Setup WebSockets
+setupSockets(await retrieveRegisteredFabricators())
 
 const app = createApp(App)
 app.use(router)
