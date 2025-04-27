d52d52470eac43cf7f62d055c3b94ca97bae7adb
dragiev1 <dragievs2020@gmail.com>
2025-04-26 23:47:04 -0400

    feat: now can delete jobs from queue in QueueList.vue (I think)

--- client/src/models/job.ts
+++ client/src/models/job.ts
@@ -1,50 +1,50 @@
 import { api } from '@/models/api'
 
 export interface Job {
-    id: number
-    name: string
-    file: File
-    download_link?: string
-    file_name_original: string
-    date?: Date
-    status?: string
-    progress?: number //store progress of job
-    gcode_num?: number //store gcode of job
-    printer: string //store printer name
-    printerid: number
-  
-    td_id: number //store td_id of job
-  
-    max_layer_height?: number //store max layer height of job
-    current_layer_height?: number //store current layer height of job
-  
-    errorid?: number
-    error?: string // store issue name
-  
-    comments?: string // store comments
-  
-    extruded?: number
-    filament?: string
-  
-    file_pause: number
-    priority?: string
-    favorite?: boolean
-    released?: number
-    job_server?: [number, Date | string, Date | string, Date | string] // this saves all the data from the backend.Only changed if there is a pause involved.
-  
-    job_client?: {
-      // this is frontend data CALCULATED based on the backend data
-      total_time: number
-      eta: number
-      elapsed_time: number
-      extra_time: number
-      remaining_time: number
-    }
-    time_started?: number
-    colorbuff?: number 
-    printer_name?: string
-    queue_selected?: boolean
+  id: number
+  name: string
+  file: File
+  download_link?: string
+  file_name_original: string
+  date?: Date
+  status?: string
+  progress?: number //store progress of job
+  gcode_num?: number //store gcode of job
+  printer: string //store printer name
+  printerid: number
+
+  td_id: number //store td_id of job
+
+  max_layer_height?: number //store max layer height of job
+  current_layer_height?: number //store current layer height of job
+
+  errorid?: number
+  error?: string // store issue name
+
+  comments?: string // store comments
+
+  extruded?: number
+  filament?: string
+
+  file_pause: number
+  priority?: string
+  favorite?: boolean
+  released?: number
+  job_server?: [number, Date | string, Date | string, Date | string] // this saves all the data from the backend.Only changed if there is a pause involved.
+
+  job_client?: {
+    // this is frontend data CALCULATED based on the backend data
+    total_time: number
+    eta: number
+    elapsed_time: number
+    extra_time: number
+    remaining_time: number
   }
+  time_started?: number
+  colorbuff?: number
+  printer_name?: string
+  queue_selected?: boolean
+}
 
 export async function getAllJobs() {
   return api('getjobs')
@@ -54,6 +54,19 @@ export async function getAllJobs() {
 //   return api('addjobtoqueue', job)
 // }
 
-export async function autoQueue(job : FormData) {
-  return api('autoqueue' , job)
+export async function autoQueue(job: FormData) {
+  return api('autoqueue', job)
+}
+
+export async function removeJob(jobarr: number[]) {
+  try {
+    const response = await api('cancelfromqueue', { jobarr })
+    if (response) {
+      return response
+    } else {
+      console.error('Response is undefined or null')
+    }
+  } catch (error) {
+    console.error(error)
+  }
 }
