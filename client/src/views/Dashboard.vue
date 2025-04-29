4da12f8aee8d91ec177fb3b30425ff552c935385
Chris J. <shift16@protonmail.com>
2025-04-29 12:01:02 -0400

    fix: Added a hack to force the reactive state to update

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -3,6 +3,12 @@ import { onMounted } from 'vue';
 import { fabricatorList, retrieveRegisteredFabricators } from '@/models/fabricator'
 import PrintModule from '@/components/PrintModule.vue'
 import NoPrinterRobot from '@/components/NoPrinterRobot.vue'
+import router from '@/router';
+
+// Hack
+setInterval(_ => {
+  router.push('/')
+}, 1000)
 
 onMounted(async () => {
   retrieveRegisteredFabricators()
