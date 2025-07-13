386b54afb82bcd68730ce30819901743e0a45bfd
Chris J. <shift16@protonmail.com>
2025-07-13 11:29:01 -0400

    cleanup: Remove extra lines

--- server-javascript/src/flags.js
+++ server-javascript/src/flags.js
@@ -19,7 +19,5 @@ export const DEBUG_FLAGS = {
     EXTRACTOR_FAILED_MATCH: false, // Annoying
     AUTOMATIC_EXTRACTOR_GIVEN: true,
     SHOW_EXTRACTOR_RESULT: true,
-    NO_NAMED_CAPTURE_GROUPS: true,
-
-
+    NO_NAMED_CAPTURE_GROUPS: true
 };
