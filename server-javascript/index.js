5295a8b019336af4824e9ce8a016851d4043dbc4
Ari Yeger <yegera1@newpaltz.edu>
2025-07-14 18:00:47 -0400

    feat: set up Express server with device and user routes, database connection, and error handling

--- server-javascript/index.js
+++ server-javascript/index.js
@@ -1,5 +1,41 @@
-// When we decide to publish this package, we will have to define what APIs are exposed to the user in this file
-console.log('Package is incomplete and should not be published');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
+// Import routes
+import deviceRoutes from './src/routes/deviceRoutes.js';
+import userRoutes from './src/routes/userRoutes.js';
+import db from './src/config/db.js';
+import ErrorHandler from './src/middlewares/ErrorHandler.js';
+// Initialize dotenv for environment variables
 
-export default undefined;
+dotenv.config();
+
+// Initialize database connection
+try {
+    await db.query('SELECT 1');
+    console.log('Database connection successful');
+}
+catch (error) {
+    console.error('Database connection failed:', error);
+    process.exit(1); // Exit the process if database connection fails
+}
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the 3D Printer Device Management API');
+});
+// Use routes
+app.use('/api/devices', deviceRoutes);
+app.use('/api/users', userRoutes);
+app.use(ErrorHandler);
+
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+});
