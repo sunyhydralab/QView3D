adf95a4f5b3b6ac781c2b7af25b39c3917470a61
Chris J. <shift16@protonmail.com>
2025-08-04 16:23:00 -0400

    feat: Updated prusamk3 class definition

--- server-javascript/src/serial_communication/marlin_firmware/prusamk3.js
+++ server-javascript/src/serial_communication/marlin_firmware/prusamk3.js
@@ -2,6 +2,7 @@ import { GenericMarlinFabricator } from "./generic.js";
 
 /** @todo Possibly incomplete */
 export class Prusai3MK3 extends GenericMarlinFabricator {
+    SUPPORTED_FABRICATORS = ['Prusa i3 MK3S'];
     RESPONSE_TIMEOUT = 10000;
     BOOT_TIME = 5000;
 }
