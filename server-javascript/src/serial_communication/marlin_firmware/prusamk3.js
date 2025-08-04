aedde8c63e1aa44f098976991a6dd47955c4960c
Chris J. <shift16@protonmail.com>
2025-08-04 15:20:06 -0400

    cleanup: Removed outdated properties

--- server-javascript/src/serial_communication/marlin_firmware/prusamk3.js
+++ server-javascript/src/serial_communication/marlin_firmware/prusamk3.js
@@ -4,5 +4,4 @@ import { GenericMarlinFabricator } from "./generic.js";
 export class Prusai3MK3 extends GenericMarlinFabricator {
     RESPONSE_TIMEOUT = 10000;
     BOOT_TIME = 5000;
-    INFO_CMD_EXTRACTOR = /FIRMWARE_NAME:(?<firmwareVersion>[^\s]+).+MACHINE_TYPE:(?<machineType>[^\s]+).+/;
 }
